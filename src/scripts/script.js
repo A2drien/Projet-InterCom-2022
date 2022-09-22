ced977c451750f6d9f4d774c30ff442228099bf9
A2drien <adriendubois1411@gmail.com>
2022-09-22 15:32:21 +0200

    Modification des liens

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     console.log("ready!");
 
     $('footer').load("../pages/factoring/footer.html");
-    $('header').load("../pages/factoring/menu.html");
+    $('header').load("../pages/factoring/header.html");
     $('#news').load("../pages/factoring/news.html");
 });
