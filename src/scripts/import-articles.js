bb4971952b479c51a4aa75a7a3238902ec017d00
A2drien <adriendubois1411@gmail.com>
2022-09-29 16:25:00 +0200

    JS Importation articles

---
+++ src/scripts/import-articles.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#article-1').load('../pages/articles/article-1.html');
+    $('#article-2').load('../pages/articles/article-2.html');
+    $('#article-3').load('../pages/articles/article-3.html');
+    $('#article-4').load('../pages/articles/article-4.html');
+});
