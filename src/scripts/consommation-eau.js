bb82e594bc9e0828adb1308f8a545bb298a37079
Oscar Lin <heartzephyr@gmail.com>
2022-11-06 19:50:09 +0100

    cons eau

---
+++ src/scripts/consommation-eau.js
@@ -0,0 +1,16 @@
+let Eppers = 4000 ;
+let Epdouche = 6 ;
+let nbJ = 30;
+
+$(document).ready(function () {
+    $('button').click(ConsEau);
+});
+
+function ConsEau(){
+    let nbPers = $('#nbP').val();
+    let minD = $('#minD').val();
+    let EpMois = Eppers * nbPers + Epdouche * minD * nbJ;
+
+    $('#EstiEau').text('Vous allez consommer en moyenne ' + EpMois + ' litre par mois.');
+}
+
