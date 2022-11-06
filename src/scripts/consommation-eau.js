cc86b82da790639b012a3cb12bf26fe71903d8b5
A2drien <adriendubois1411@gmail.com>
2022-11-06 19:56:10 +0100

    Renommer variables

--- src/scripts/consommation-eau.js
+++ src/scripts/consommation-eau.js
@@ -1,16 +1,16 @@
-let Eppers = 4000 ;
-let Epdouche = 6 ;
-let nbJ = 30;
+const EAU_PAR_PERSONNE_PAR_MOIS = 4000;
+const EAU_PAR_DOUCHE_PAR_MINUTE = 6;
+const NOMBRE_JOURS_MOIS = 30;
 
 $(document).ready(function () {
     $('button').click(ConsEau);
 });
 
-function ConsEau(){
+function ConsEau() {
     let nbPers = $('#nbP').val();
-    let minD = $('#minD').val();
-    let EpMois = (Eppers * nbPers) + (Epdouche * minD * nbJ) ;
+    let minutesDouche = $('#minD').val();
+    let EauParMois = (EAU_PAR_PERSONNE_PAR_MOIS * nbPers) + (EAU_PAR_DOUCHE_PAR_MINUTE * minutesDouche * NOMBRE_JOURS_MOIS);
 
-    $('#EstiEau').text('Vous allez consommer en moyenne ' + EpMois + ' litres par mois.');
+    $('#EstiEau').text('Vous allez consommer en moyenne ' + EauParMois + ' litres par mois.');
 }
 
